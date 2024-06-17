dcd862afbdc962389accb7010f5f2359d526364b
Leandro A. F. Fernandes <laffernandes@ic.uf.br>
2024-06-16 21:29:43 -0300

    Pequenos ajustes

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import glob, os, subprocess, sys
 DATASET_DIR: Final = os.path.join('data', 'dataset')
 OUTPUT_DIR: Final = os.path.join('data', 'output')
 
-for filepath in ['data\\dataset\\video - iacs-camera-01 - 2024.06.10 15.59.mp4']: # glob.glob(os.path.join(DATASET_DIR, '*.mp4')): #Debug ['data\\dataset\\video - iacs-camera-01 - 2024.06.10 15.59.mp4']
+for filepath in glob.glob(os.path.join(DATASET_DIR, '*.mp4')): #Debug ['data\\dataset\\video - iacs-camera-01 - 2024.06.10 15.59.mp4']
     root = os.path.splitext(os.path.basename(filepath))[0]
     subprocess.run([sys.executable, '-m', 'licenceplate',
         '--server', 'some_address',
         '--video', filepath,
         '--name', root,
-        # '--output', os.path.join(OUTPUT_DIR, f'{root}.avi'),
+        '--output', os.path.join(OUTPUT_DIR, f'{root}.avi'),
     ])
